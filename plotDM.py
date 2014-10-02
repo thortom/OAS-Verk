Sign fixed in the dynamic model

plotDM.py
@@ -9,9 +9,10 @@ if __name__ == '__main__':
 	title = np.loadtxt(file_name, dtype=str, delimiter=",")
 	data = np.loadtxt(file_name, skiprows=1, delimiter=",")
 
-	print "title = ", title
-
 	for idx, item in enumerate(data[0,:]):
+		if(idx == 0):
+			#skip time
+			continue
 		plot(data[:,0], data[:,idx], label=title[0, idx])
 	legend()
 	show()