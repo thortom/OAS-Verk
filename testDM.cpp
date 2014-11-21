e1f8528bc3674f6b30c3621b70433b186ee76fb2
thortom <thortomasarson@gmail.com>
2014-11-21 16:26:58 +0000

    adding test figure

--- testDM.cpp
+++ testDM.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
     double convToDeg = 180.0/3.1415;
 
     double t = 0.0;
-    const double dt = 0.1;
+    const double dt = 0.04;
     double surge = 1.5;
     double initState[12*1] = {surge , 0.0 , 0.0 , 0.0, 0.0, 0.0, 0.0, 0.0 , 0.0 , 0.0, 0.0, 0.0};
     AUVModel* model = new AUVModel(); //initState, t, dt);
@@ -20,22 +20,22 @@ int main(int argc, char **argv)
     // The system seems to be unstable
     std::ofstream file;
     file.open ("DynamicModel.csv");
-    file << "t" << ',' << "u" << ',' << "v" << ',' << "w" << ',' << "p" << ',' << "q" << ',' << "r" << ',' << "y" << ',' << "z" << std::endl;//',' << "phi" << ',' << "theta" << ',' << "psi" << std::endl;
+    file << "t" << ',' << "u" << ',' << "v" << ',' << "w" << ',' << "p" << ',' << "q" << ',' << "r" << ',' << "y" << ',' << "z" << ',' << "phi" << ',' << "theta" << ',' << "psi" << std::endl;
     //file << "t" << ',' << "u" << ',' << "v" << ',' << "w" << std::endl;
-    int numbSteps = 1000;
-    for(size_t i=0 ; i<numbSteps ; ++i,t+=dt )
+    int numbSteps = 100;
+    for(size_t i=0 ; t<numbSteps ; ++i,t+=dt )
     {
         integrator->dostep(model);      // TODO: change to model->doWork() {upDataMatrixes(matJ, matTa), integrator->dostep(model)}
         //model->doWork();
         file << t << ',' << model->surge() << ',' << model->sway() << ',' << model->heave() << ',' << model->roll()*convToDeg << ',' << model->pitch()*convToDeg << ',' << model->yaw()*convToDeg;
-        file << ',' << model->yPos() << ',' << model->zPos() << std::endl;//',' << model->phiRads()*convToDeg << ',' << model->thetaRads()*convToDeg << ',' << model->psiRads()*convToDeg << std::endl;
+        file << ',' << model->yPos() << ',' << model->zPos() << ',' << model->phiRads()*convToDeg << ',' << model->thetaRads()*convToDeg << ',' << model->psiRads()*convToDeg << std::endl;
         //file << t << ',' << model->surge() << ',' << model->sway() << ',' << model->heave() << std::endl;        
         //std::cout << t << '\t' << model->surge() << '\t' << model->sway() << '\t' << model->heave() << '\t' << model->roll() << '\t' << model->pitch() << '\t' << model->yaw() << '\t' << model->xPos();
         //std::cout << '\t' << model->xPos() << '\t' << model->yPos() << '\t' << model->zPos() << '\t' << model->phiRads() << '\t' << model->thetaRads() << '\t' << model->psiRads() << std::endl;
         
         //model->input(surge/0.0025, 80*3.1415/180.0, 80*3.1415/180.0, 80*3.1415/180.0, 80*3.1415/180.0);
         
-        model->input(surge/0.0025, 0, 0, -30*3.1415/180.0, -30*3.1415/180.0); //surge/0.0025, 0, 0, 0, 0);
+        model->input(surge/0.0025, 0, 0, 60*3.1415/180.0, 60*3.1415/180.0); //surge/0.0025, 0, 0, 0, 0);
         
     }
     file.close();
